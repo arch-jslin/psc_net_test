e59d5699b580adf5417b45c34a33bbd5b0941981
Johnson Lin <arch.jslin@gmail.com>
2012-02-21 04:04:50 -0500

    update README and build script for linux

--- build_linux.lua
+++ build_linux.lua
@@ -2,3 +2,4 @@ os.execute [[gcc -I/usr/local/include/luajit-2.0 -L/usr/local/lib -lluajit-5.1 -
 
 os.execute [[mv psc_net_test bin/Release]]
 
+os.execute [[export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH]]
