1adcaf23d7f3e61451cb8ee7414dfd3b43e2bcbb
Johnson Lin <arch.jslin@gmail.com>
2012-03-19 02:13:48 -0400

    update new build script

--- build_linux.lua
+++ build_linux.lua
@@ -1,4 +1,4 @@
-os.execute [[gcc -I/usr/local/include/luajit-2.0 -L/usr/local/lib -lluajit-5.1 -lboost_thread-mt -O1 -Os -Wl,-E -o psc_net_test lua_utils.cpp main.cpp]]
+os.execute [[gcc -I/usr/local/include/luajit-2.0 -L/usr/local/lib -lstdc++ -lluajit-5.1 -lboost_thread -O1 -Os -Wl,-E -o psc_net_test lua_utils.cpp main.cpp]]
 
 os.execute [[mv psc_net_test bin/Release]]
 
