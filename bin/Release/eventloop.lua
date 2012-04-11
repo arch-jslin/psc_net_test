9894d6b3dd600df851675a640a84e677b035d25f
marvinjeng <marvinjeng@gmail.com>
2012-04-11 11:25:59 +0800

    test

--- bin/Release/eventloop.lua
+++ bin/Release/eventloop.lua
@@ -74,9 +74,9 @@ end
 net.matcher = function(ip, port)
   local function foo()
     --dump('connect to '..ip..":"..port)
-    net.conn_matcher = net.host:connect(ip..":"..port)
+    --net.conn_matcher = net.host:connect(ip..":"..port)
     --net.conn_matcher = net.host:connect("localhost:12345")
-    --net.conn_matcher = net.host:connect(IP_LOCAL..":54321")
+    net.conn_matcher = net.host:connect(IP_LOCAL..":54321")
   end
 
   local ok, err = pcall(foo)
@@ -244,6 +244,7 @@ function run(sc_flag)
 
   prep.setup(net, game)
   play.setup(net, game)
+    -- for testing
 
   while not C.check_quit() do
 
