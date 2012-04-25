1ec41378c82c8f5046907a5e11e141e8f4808c96
Johnson Lin <arch.jslin@gmail.com>
2012-04-25 16:42:50 +0800

    removed threading

--- main.cpp
+++ main.cpp
@@ -5,7 +5,6 @@
 #include <deque>
 #include <cstdio>
 #include <cstdlib>
-#include <boost/thread/thread.hpp>
 #include <boost/thread/mutex.hpp>
 #include <boost/tr1/functional.hpp>
 #include "lua_utils.hpp"
@@ -15,8 +14,6 @@
 #else
 #define APIEXPORT
 #endif
-
-boost::mutex      MQ_MUTEX;
 
 std::deque<int>   MQ;
 lua_State*        L = 0;
@@ -36,25 +33,38 @@ enum GameState{
 NetState  N_STATE = N_DEFAULT;
 GameState G_STATE = G_DEFAULT;
 
+void end_lua();
 void start_lua(char ch)
 {
     L = luaL_newstate();
     luaL_openlibs(L);
     Lua::run_script(L, "eventloop.lua");
 
-    if( ch == 's' ){
-        Lua::call(L, "init", 1); //server
-        Lua::call(L, "run");
-    }else if(ch=='c'){
-        Lua::call(L, "init", 2); //client
-        Lua::call(L, "run");
+    bool get_lobby = false;
+    if( ch == 's' ) {
+        get_lobby = Lua::call_R<bool>(L, "init", 1); //server
     }
+    else {
+        get_lobby = Lua::call_R<bool>(L, "init", 2); //client
+    }
+    if ( !get_lobby ) {
+        printf("C: notified by lua that connecting to lobby server failed.\n");
+        end_lua();
+    }
+}
 
-    printf("C: Returned back to C.\n");
-    {
-        boost::mutex::scoped_lock l(MQ_MUTEX);
-        MQ.clear();
-    }
+void process_lua(int cmd)
+{
+    Lua::call(L, "run", cmd);
+}
+
+void end_lua()
+{
+    Lua::call(L, "dtor");
+    printf("C: main loop ended.\n");
+
+    MQ.clear();
+
     N_STATE = N_DEFAULT;
     LUA_QUIT =  false;
     lua_close(L);
@@ -79,7 +89,6 @@ extern "C" {
     }
 
     APIEXPORT int poll_from_C() {
-        boost::mutex::scoped_lock l(MQ_MUTEX);
         if( !MQ.empty() ) {
             int front = MQ.front();
             MQ.pop_front();
@@ -110,36 +119,40 @@ void input_loop()
 int main()
 {
     using std::tr1::bind;
-    boost::thread* thLua  = 0;
     boost::thread  thInput( input_loop );
 
     while ( CH != 3 ) { //ctrl-c
-#if defined(WIN32) || defined(__WIN32__)
-        Sleep(250);
-#else   //NOT WIN32
-        usleep(250000);
-#endif  //WIN32
-        if( CH == 's' || CH == 'c' ) {
-            if( !L ) {
-                if( thLua ) {
-                    delete thLua;
-                    thLua = 0;
-                }
-                thLua = new boost::thread( bind(start_lua, CH) );
+        if( !L ) {
+            if( CH == 's' || CH == 'c' ) {
+                start_lua(CH);
             }
         }
-        else if( CH == 'p' ) {
-            if( L ) {
+        else {
+            if( CH == 'p' ) {
                 LUA_QUIT = true;
+                end_lua();
+                continue;
             }
-        }
-        else if( CH >= '1' && CH <= '4' ) {
-            if( L && thLua && N_STATE == N_MATCHED ) {
-                boost::mutex::scoped_lock l(MQ_MUTEX);
-                MQ.push_back(CH);
+            else if( CH >= '1' && CH <= '4' ) {
+                if( N_STATE == N_MATCHED ) {
+                    MQ.push_back(CH);
+                }
+            }
+            if( !MQ.empty() ) {
+                int front = MQ.front();
+                MQ.pop_front();
+                process_lua(front);
+            } else {
+                process_lua(0);
             }
         }
         CH = 0; //reset CH here
+#if defined(WIN32) || defined(__WIN32__)
+        Sleep(1);
+#else   //NOT WIN32
+        usleep(1000);
+#endif  //WIN32
     }
+    end_lua();
     return 0;
 }
